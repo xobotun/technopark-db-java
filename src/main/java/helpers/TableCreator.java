7fb43ed8ecb067fc9b446cbe45afde3ae56952b2
Paul Maminov <xobotun@ya.ru>
2016-04-04 23:03:14 +0300

    Probably fixed issue with cyrillic symbols.

--- src/main/java/helpers/TableCreator.java
+++ src/main/java/helpers/TableCreator.java
@@ -43,7 +43,8 @@ public class TableCreator {
         Statement statement = null;
         try {
             statement = connection.createStatement();
-
+            
+            statement.execute("SET NAMES 'utf8';");
             statement.execute("DROP USER IF EXISTS " + DBConnectionManager.LOGIN_DOMAIN);
             statement.execute("CREATE USER " + DBConnectionManager.LOGIN_DOMAIN + " IDENTIFIED BY \"" + DBConnectionManager.PASSWORD + "\";");
 
